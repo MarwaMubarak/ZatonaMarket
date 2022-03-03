1809645f4c3e6ff33a21269fdd6082cfd09d6fcd
MarwaMubarak <marwamubarak6@gmail.com>
2022-03-03 21:50:27 +0200

    Update fruits_view.dart

--- lib/views/fruits_view.dart
+++ lib/views/fruits_view.dart
@@ -7,13 +7,15 @@ class FruitsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Section(
-      Colors.purple.shade50,
-      Colors.purple.shade900,
-      'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/pomegranate.jpg','Pomegranate',11.99,
-      'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/kiwi.jpg','kiwi',29.99,
-      'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/orange.jpg','Orange',5.0,
-      'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/apple.jpg','Apple',25.5,
+    return Scaffold(
+      body: Section(
+        Colors.purple.shade50,
+        Colors.purple.shade900,
+        'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/pomegranate.jpg','Pomegranate',11.99,
+        'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/kiwi.jpg','kiwi',29.99,
+        'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/orange.jpg','Orange',5.0,
+        'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/apple.jpg','Apple',25.5,
+      ),
     );
   }
 }
