7f8c8c2756b95f1f362b5780e2992f79d34fb803
MarwaMubarak <marwamubarak6@gmail.com>
2022-03-03 21:50:22 +0200

    Update dairy_view.dart

--- lib/views/dairy_view.dart
+++ lib/views/dairy_view.dart
@@ -7,13 +7,15 @@ class DairyView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Section(
-      Colors.blue.shade100,
-      Colors.blue.shade900,
-      'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/milk.jpg','Milk',14.5,
-      'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/eggs.jpg','Egg',1.80,
-      'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/butter.jpg','Butter',80.5,
-      'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/cheese.jpg','Cheese',75.0,
+    return Scaffold(
+        body: Section(
+        Colors.blue.shade100,
+        Colors.blue.shade900,
+        'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/milk.jpg','Milk',14.5,
+        'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/eggs.jpg','Egg',1.80,
+        'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/butter.jpg','Butter',80.5,
+        'C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/cheese.jpg','Cheese',75.0,
+      ),
     );
   }
 }
