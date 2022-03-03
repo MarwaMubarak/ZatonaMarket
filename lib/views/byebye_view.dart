4164c3ec4c65ee9384d5543651736a72f815cea6
MarwaMubarak <marwamubarak6@gmail.com>
2022-03-03 21:50:17 +0200

    Update byebye_view.dart

--- lib/views/byebye_view.dart
+++ lib/views/byebye_view.dart
@@ -5,21 +5,23 @@ class ByeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image.asset('C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/byebye.jpg'),
-            Column(
-              children: const [
-                Text('Bye Bye ..',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.indigo,fontSize: 30),),
-                Text('Visit us again :)',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.indigo,fontSize: 30),),
-              ],
-            ),
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset('C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/byebye.jpg'),
+              Column(
+                children: const [
+                  Text('Bye Bye ..',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.indigo,fontSize: 30),),
+                  Text('Visit us again :)',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.indigo,fontSize: 30),),
+                ],
+              ),
 
-          ],
+            ],
+          ),
         ),
       ),
     );
