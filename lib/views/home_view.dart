eb3d20e64b5e2ba9c8bf1488a810e00edef1bf09
MarwaMubarak <marwamubarak6@gmail.com>
2022-03-03 21:50:32 +0200

    Update home_view.dart

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -6,47 +6,49 @@ class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey.shade100,
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.green.shade900,
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    height: 40,
-                    width: 280,
-                    child: const Center(child: Text('Zatona Market',style:TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 35,),))
+    return Scaffold(
+      body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.green.shade900,
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      height: 40,
+                      width: 280,
+                      child: const Center(child: Text('Zatona Market',style:TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 35,),))
+                  ),
                 ),
-              ),
-              const Text(' '),
-              const Text(' '),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Card1('C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/meats.jpg', 'butcher',1),
-                  Card1('C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/fruits.jpg', 'Fruits',2),
-                ],
-              ),
-              const Text(' '),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Card1('C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/dairy.jpg', 'Dairy',3),
-                  Card1('C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/vegtables.jpg', 'Vegtables',4),
+                const Text(' '),
+                const Text(' '),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Card1('C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/meats.jpg', 'butcher',1),
+                    Card1('C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/fruits.jpg', 'Fruits',2),
+                  ],
+                ),
+                const Text(' '),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Card1('C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/dairy.jpg', 'Dairy',3),
+                    Card1('C:/Users/Legion 5/StudioProjects/zatona_market/assets/images/vegtables.jpg', 'Vegtables',4),
 
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-      );
+    );
   }
 }
